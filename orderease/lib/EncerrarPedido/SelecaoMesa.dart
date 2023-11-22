07e4db38df9309134a3647e46b1bb5915c65ff41
CarlosArantes53 <74297144+CarlosArantes53@users.noreply.github.com>
2023-11-22 13:20:13 -0300

    Seleção das mesas

--- orderease/lib/EncerrarPedido/SelecaoMesa.dart
+++ orderease/lib/EncerrarPedido/SelecaoMesa.dart
@@ -1,5 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'EncerrarComanda.dart'; // Importe a classe EncerrarPedidos
+import 'package:orderease/EncerrarPedido/EncerrarComanda.dart';
 
 class SelecaoMesa extends StatelessWidget {
   const SelecaoMesa({Key? key}) : super(key: key);
@@ -21,48 +22,62 @@ class SelecaoMesa extends StatelessWidget {
       body: Center(
         child: Container(
           padding: const EdgeInsets.all(16),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  _navigateToEncerrarPedidos(context, 1); // Mesa 1
-                },
-                style: ElevatedButton.styleFrom(
-                  padding: EdgeInsets.all(20),
+          child: FutureBuilder(
+            future: FirebaseFirestore.instance.collection('pedidos').get(),
+            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return CircularProgressIndicator();
+              }
+
+              if (snapshot.hasError) {
+                return Text('Erro: ${snapshot.error}');
+              }
+
+              if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                return Text('Nenhuma mesa encontrada.');
+              }
+
+              List<Widget> mesaButtons = [];
+
+              snapshot.data!.docs.forEach((doc) {
+                var numeroMesa = doc['mesa'];
+                mesaButtons.add(
+                  ElevatedButton(
+                    onPressed: () {
+                      _navigateToEncerrarPedidos(context, int.parse(numeroMesa));
+                    },
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.all(20),
+                    ),
+                    child: Text(
+                      'Mesa $numeroMesa',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                  ),
+                );
+                mesaButtons.add(const SizedBox(height: 16));
+              });
+
+              mesaButtons.add(
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context); // Voltar para a tela anterior
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.all(20),
+                  ),
+                  child: const Text(
+                    'Voltar',
+                    style: TextStyle(fontSize: 30),
+                  ),
                 ),
-                child: Text(
-                  'Mesa 1',
-                  style: TextStyle(fontSize: 30),
-                ),
-              ),
-              const SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () {
-                  _navigateToEncerrarPedidos(context, 2); // Mesa 2
-                },
-                style: ElevatedButton.styleFrom(
-                  padding: EdgeInsets.all(20),
-                ),
-                child: Text(
-                  'Mesa 2',
-                  style: TextStyle(fontSize: 30),
-                ),
-              ),
-              const SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context); // Voltar para a tela anterior
-                },
-                style: ElevatedButton.styleFrom(
-                  padding: EdgeInsets.all(20),
-                ),
-                child: const Text(
-                  'Voltar',
-                  style: TextStyle(fontSize: 30),
-                ),
-              ),
-            ],
+              );
+
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: mesaButtons,
+              );
+            },
           ),
         ),
       ),
